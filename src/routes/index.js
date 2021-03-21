9588bbcdc1cb9f3b3226f88ff3138ece9680753a
fiferro <filipe.ferro@gmail.com>
2021-03-21 14:07:58 -0300

    Ajsutes para PR

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,15 +12,19 @@ router.get("/CNPJ/:CNPJ", function (req, res) {
     console.log(idEstabelecimento);
     var request = require('request');
     var options = {
-      'method': 'GET',
-      'url': 'https://www.receitaws.com.br/v1/cnpj/'+ idEstabelecimento ,
-      'headers': {
-      }
+        'method': 'GET',
+        'url': 'https://www.receitaws.com.br/v1/cnpj/' + idEstabelecimento,
+        'headers': {
+        }
     };
     request(options, function (error, response) {
-      if (error) throw new Error(error);
-      console.log(response.body);
+        if (error) throw new Error(error);
+        console.log(response.body);
+        res.status(200).send({
+            response
+        });
     });
+
 });
 
 module.exports = router;
